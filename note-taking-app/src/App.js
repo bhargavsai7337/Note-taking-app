0adeb37e75c1053918fd310495b3a895fdf9e0a3
bhargavsai7337 <bhargavsaich7337@gmail.com>
2023-04-02 21:42:30 -0400

    secound commit

--- note-taking-app/src/App.js
+++ note-taking-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import{ nanoid } from 'nanoid'
+import NotesList from "./components/Notes_List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App =()=> {
+  const [notes, setNotes]=useState([
+  {
+    text:"This is my first note!",
+    date: "04/02/2023",
+  },
+  {
+    text:"This is my second note!",
+    date: "04/02/2023",
+  },
+  {
+    text:"This is my third note!",
+    date: "04/02/2023",
+  }
+]);
+  return <div className="container">
+    <NotesList notes={notes} />
+  </div>
+};
 
 export default App;
