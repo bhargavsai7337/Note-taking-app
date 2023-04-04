ae13d3f6efcdb7d8303198be96611504c4e5516c
bhargavsai7337 <bhargavsaich7337@gmail.com>
2023-04-04 00:10:51 -0400

    Third commit

--- note-taking-app/src/App.js
+++ note-taking-app/src/App.js
@@ -5,20 +5,36 @@ import NotesList from "./components/Notes_List";
 const App =()=> {
   const [notes, setNotes]=useState([
   {
+    id: nanoid(),
     text:"This is my first note!",
     date: "04/02/2023",
   },
   {
+    id: nanoid(),
     text:"This is my second note!",
     date: "04/02/2023",
   },
   {
+    id: nanoid(),
     text:"This is my third note!",
     date: "04/02/2023",
   }
 ]);
+
+const AddNote =(text) => {
+  const date=new Date();
+  const newNote ={
+    id:nanoid(),
+    text:text,
+    date: date.toLocaleDateString()
+  }
+
+  const newNotes = [...notes,newNote];
+  setNotes(newNotes);
+}
+
   return <div className="container">
-    <NotesList notes={notes} />
+    <NotesList notes={notes} handleAddNote={AddNote}/>
   </div>
 };
 
